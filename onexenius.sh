3307778004d41a6ea1ab0444fc7446ccdcbe24f3
Philippe Jean Guillaumie <batden@sfr.fr>
2017-03-29 11:00:44 +0200

    Bump script version

--- onexenius.sh
+++ onexenius.sh
@@ -50,7 +50,7 @@ DISTRIBUTOR=$(lsb_release -i | cut -f2)
 CODE=${LANG:0:2}
 GHUB="https://raw.githubusercontent.com/batden/git-enlightened/master"
 VER_ONLINE=$(wget --quiet -S -O - $GHUB/13 |& sed '$!d')
-CURVERNUM="13.8"
+CURVERNUM="13.9"
 
 #~ (Color output)
 BLD="\e[1m"     #~ (Bold text)
